14ab0158dcf111a03b65f5d8fdbf11706db3f28c
Per Hultqvist <per@softteam.se>
2021-07-15 09:41:31 +0200

    version 2.21 - bug fix - failed downloads #2

--- cmd/softtube/constants.go
+++ cmd/softtube/constants.go
@@ -2,7 +2,7 @@ package main
 
 //const constAppID = "com.github.hultan.softtube"
 const constAppTitle = "SoftTube"
-const constAppVersion = "2.20"
+const constAppVersion = "2.21"
 const constDateLayout = "2006-01-02"
 
 // LISTSTORE COLUMNS
