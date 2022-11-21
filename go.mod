32bf3edb7365dbf7b7040cc486e01e2b02c4047c
per <per@softteam.se>
2022-11-21 11:47:32 +0100

    softtube.shrink improvements

--- go.mod
+++ go.mod
@@ -5,5 +5,11 @@ go 1.18
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gotk3/gotk3 v0.6.1
-	github.com/hultan/softteam v1.2.7
+	github.com/hultan/softteam v1.3.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- tools/softtube.shrink/main.go
+++ tools/softtube.shrink/main.go
@@ -2,51 +2,74 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/gotk3/gotk3/gdk"
 
+	"github.com/hultan/softteam/framework"
 	core "github.com/hultan/softtube/internal/softtube.core"
 )
 
 const (
-	largeFileSize = 50000		// 50 kb is an acceptable size
-	bestQuality = 100			// JPEG quality
+	largeFileSize = 50000 // 50 kb is an acceptable size
+	bestQuality   = 100   // JPEG quality
 )
 
 var (
+	logger *framework.Logger
 	config *core.Config
 )
 
 func main() {
-	// Init config file
-	config = new(core.Config)
-	err := config.Load("main")
+	var err error
+
+	fw := framework.NewFramework()
+	logger, err = fw.Log.NewStandardLogger("/softtube/log/softtube.shrink.log")
 	if err != nil {
-		fmt.Println("ERROR (Open config) : ", err.Error())
+		_, _ = fmt.Fprintln(os.Stderr, "Failed to open log file : ", err)
+		os.Exit(1)
+	}
+
+	logger.Info.Println("")
+	logger.Info.Println("----------------------")
+	logger.Info.Println("Softtube.shrink start!")
+	logger.Info.Println("----------------------")
+	logger.Info.Println("")
+
+	// Init config file
+	config = &core.Config{}
+	err = config.Load("main")
+	if err != nil {
+		logger.Error.Println("Failed to load the SoftTube config : ")
+		logger.Error.Println(err)
+		logger.Error.Println("Exiting...")
 		os.Exit(1)
 	}
 
 	var thumbnailPath = config.ServerPaths.Thumbnails
-	// var thumbnailPath = "/softtube/thumb/"
 
 	// Find large files
 	large, err := findLargeFiles(thumbnailPath)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error.Println("Failed to find large files : ")
+		logger.Error.Println(err)
+		logger.Error.Println("Exiting...")
+		os.Exit(1)
 	}
+
+	logger.Info.Printf("Found %d large files.\n", len(large))
+	logger.Info.Println("")
 
 	// Shrink files
 	for _, fileName := range large {
-		fullPath := fmt.Sprintf("%s/%s",thumbnailPath , fileName)
+		fullPath := path.Join(thumbnailPath, fileName)
 
-		log.Println("Shrinking file : ", fullPath)
-
+		// Log successful shrinks, errors are logged in shrinkFile()
 		err = shrinkFile(fullPath)
-		if err != nil {
-			log.Println(err)
+		if err == nil {
+			logger.Info.Println("Successfully shrunk file : ", fullPath)
 		}
 	}
 }
@@ -54,16 +77,19 @@ func main() {
 func shrinkFile(fullPath string) error {
 	thumbnail, err := gdk.PixbufNewFromFile(fullPath)
 	if err != nil {
+		logger.Error.Println("Failed to load file : ", fullPath)
 		return err
 	}
 
 	thumbnail, err = thumbnail.ScaleSimple(160, 90, gdk.INTERP_BILINEAR)
 	if err != nil {
+		logger.Error.Println("Failed to scale file : ", fullPath)
 		return err
 	}
 
 	err = thumbnail.SaveJPEG(fullPath, bestQuality)
 	if err != nil {
+		logger.Error.Println("Failed to save file : ", fullPath)
 		return err
 	}
 
@@ -72,11 +98,13 @@ func shrinkFile(fullPath string) error {
 
 func findLargeFiles(root string) ([]string, error) {
 	var f []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() && info.Size() > largeFileSize {
-			f = append(f, info.Name())
-		}
-		return nil
-	})
+	err := filepath.Walk(
+		root, func(path string, info os.FileInfo, err error) error {
+			if !info.IsDir() && info.Size() > largeFileSize {
+				f = append(f, info.Name())
+			}
+			return nil
+		},
+	)
 	return f, err
 }
