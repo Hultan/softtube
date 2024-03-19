5a83f25d41ac8aee365e1fed953224fb566837df
per <per@softteam.se>
2024-03-19 17:01:57 +0100

    softtube.update - Changed title clean regex

--- tools/softtube.update/main.go
+++ tools/softtube.update/main.go
@@ -282,7 +282,7 @@ func handleNewVideo(video database.Video, waitGroup *sync.WaitGroup) error {
 }
 
 func clean(title string) string {
-	re := regexp.MustCompile("[^[:ascii:]åäöÅÄÖ]")
+	re := regexp.MustCompile("^\\p{Cc}")
 
 	return re.ReplaceAllLiteralString(title, "")
 }
